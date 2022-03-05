44cc29626de38e10dc8ce249a081c76f6c9dc697
Joshua Graber <joshua.d.graber@gmail.com>
2022-03-05 14:01:25 -0500

    "restore app on new machine"

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Home from './components/Home';
 import MainHeader from './components/MainHeader';
 import About from './components/About';
 import Work from './components/Work';
-import Footer from './components/Footer'
+import Footer from './components/Footer';
 
 import BlogHeader from './components/BlogHeader';
 import BlogMain from './components/BlogMain';
@@ -24,38 +24,36 @@ import Post from './components/Post';
 import { StyledPageWrap } from './components/Styled/StyledPageWrap';
 import DarkModeToggle from './components/DarkModeToggle';
 
-
 const App = () => {
-  const { currentColors } = usePortfolioContext().darkMode;
+	const { currentColors } = usePortfolioContext().darkMode;
 
-  return (
-    <Router>
-      <ScrollToTop />
+	return (
+		<Router>
+			<ScrollToTop />
 
-          <Route exact path='/' component={Home} />
+			<Route exact path="/" component={Home} />
 
-      <Route path={['/about', '/work']}> 
-        <MainHeader />
-        <DarkModeToggle />
-          <StyledPageWrap currentColors={currentColors}>
-            <Route path='/about' component={About} />
-            <Route path='/work' component={Work} />
-          </StyledPageWrap>
-        <Footer />
-      </Route>
+			<Route path={['/about', '/work']}>
+				<MainHeader />
+				<DarkModeToggle />
+				<StyledPageWrap currentColors={currentColors}>
+					<Route path="/about" component={About} />
+					<Route path="/work" component={Work} />
+				</StyledPageWrap>
+				<Footer />
+			</Route>
 
-      <Route path='/io'>
-        <BlogHeader currentColors={currentColors}/>
-        <DarkModeToggle />
-        <StyledPageWrap currentColors={currentColors}>
-          <Route exact path='/io' component={BlogMain} />
-          <Route path='/io/:slug' component={Post} />
-        </StyledPageWrap>
-        <Footer />
-      </Route>
-
-    </Router>
-  );
-}
+			<Route path="/io">
+				<BlogHeader currentColors={currentColors} />
+				<DarkModeToggle />
+				<StyledPageWrap currentColors={currentColors}>
+					<Route exact path="/io" component={BlogMain} />
+					<Route path="/io/:slug" component={Post} />
+				</StyledPageWrap>
+				<Footer />
+			</Route>
+		</Router>
+	);
+};
 
 export default App;
